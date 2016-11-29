6f00c50f4659c13d0c8299385b27c8bb6ee12fcd
Justin Inouye <inouju@gmail.com>
2016-11-29 15:17:19 -0800

    Added jquery dependency and Map skeleton

---
+++ src/Map.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import $ from 'jquery';
+
+class Map extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: props.initialColor
+    };
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Map;
