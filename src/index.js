197852947e70cee43aa28b72a85df430b278cceb
tiff <tzchen@uw.edu>
2016-12-06 14:22:15 -0800

    added files for new components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import firebase from 'firebase';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { IndexRoute, Router, Route, Link, hashHistory } from 'react-router';
 import './css/index.css';
 import LandingPage from './LandingPage';
 import TweetMap from './TweetMap';
+
+ // Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDT9jJgY7WsgfCYI5yIHu0Ww0W6YF4hQgA",
+	authDomain: "fidjjit.firebaseapp.com",
+	databaseURL: "https://fidjjit.firebaseio.com",
+	storageBucket: "fidjjit.appspot.com",
+	messagingSenderId: "50404078496"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
     <Router history={hashHistory}>
