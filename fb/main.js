f3ed62ec0953e261f036aa6a172294b51de7d48e
tiff <tzchen@uw.edu>
2016-12-06 10:22:03 -0800

    got access token ty justin

--- fb/main.js
+++ fb/main.js
@@ -15,12 +15,24 @@ $(function() {
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
 
+    var client_id = "1704747783175388";
+    var client_secret = "fa448beb412c56421fdb1f2c3cec65f1";
+
     $.get(
-        /oauth/access_token
-        ?client_id={'1704747783175388'}
-        &client_secret={'fa448beb412c56421fdb1f2c3cec65f1'}
-        &grant_type=client_credentials
+        "https://graph.facebook.com/oauth/access_token?client_id="
+        + client_id + "&client_secret=" 
+        + client_secret + "&grant_type=client_credentials"
     )
+
+    FB.api(
+        '/fasasuw/events',
+        'GET',
+        {},
+        function(response) {
+            // code goes here
+        }
+    );
+
 
     
     $('main').append()
