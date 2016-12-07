25b609cfa9c7af961c2e09fd59fcb84176a521eb
tiff <tzchen@uw.edu>
2016-12-07 00:03:09 -0800

    added stuff to SearchHistory.js

--- src/SearchHistory.js
+++ src/SearchHistory.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-var url = "52.8.18.242/tweets";
+var SearchHistory = React.createClass({
+	getInitialState:function() {
 
-$.get(url).then(function(data) {
-	console.log(data);
+	},
+
+	filter:function(e) {
+		console.log(e.target.value);
+		this.setState({searchString:e.target.value});
+	},
+	render:function() {
+		var url = "52.8.18.242/tweets";
+
+		$.get(url).then(function(data) {
+			console.log(data);
+		});
+		
+		return (
+			<div>
+                <input onChange={this.filter} placeholder="Search employees"/>
+                <EmployeeTable data={employees}/>
+            </div>
+		)
+	}
+
 });
+
+export default SearchHistory;
