cc3d681924e327eef77616d4e42a11b37d0d2ac6
Justin Inouye <inouju@gmail.com>
2016-12-08 15:48:55 -0800

    Added AJAX call to server

--- src/TweetMap.js
+++ src/TweetMap.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-
 import { Map, CircleMarker, Popup, TileLayer } from 'react-leaflet';
+import $ from 'jquery';
+
 require('./css/map.css');
 
 class TweetMap extends Component {
+
+  constructor(props) {
+    super(props);
+    var comp = this;
+    $.get('http://52.8.18.242/tweets.php', function(data) {
+      var arr = data.split(/\n/);
+      arr.forEach(function(elem) {
+        //console.log(elem);
+        console.log(JSON.parse(elem).coordinates);
+      });
+    });
+  }
 
   render() {
 
@@ -37,6 +50,9 @@ class TweetMap extends Component {
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          {
+
+          }          
           <CircleMarker center={position}>
             <Popup>
               <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
