4d8096ae61e638a0eb94c71dfed4bf71ae233352
Justin Inouye <inouju@gmail.com>
2016-12-08 12:46:01 -0800

    Reimplement map with leafletinstead of d3

--- src/TweetMap.js
+++ src/TweetMap.js
@@ -1,89 +1,52 @@
 import React, { Component } from 'react';
-
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-//import $ from 'jquery';
-require('./css/bubble.css');
 
-var MapBubble = require('react-d3-map-bubble').MapBubble;
-var topojson = require('topojson');
-var us = require('./data/us.json');
+import { Map, CircleMarker, Popup, TileLayer } from 'react-leaflet';
+require('./css/map.css');
 
 class TweetMap extends Component {
 
   render() {
-
-    var width = 960;
-    var height = 600;
-
-    // data should be a MultiLineString
-    var dataStates = topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; });
-    /// data should be polygon
-    var dataCounties = topojson.feature(us, us.objects.nation);
-
-    // class
-    var meshClass = 'border';
-    var polygonClass = 'land';
-
-    // domain
-    var domain = {
-      scale: 'sqrt',
-      domain: [0, 1e6],
-      range: [0, 15]
-    };
-
-    var circles = topojson.feature(us, us.objects.counties).features
-      .sort(function(a, b) { return b.properties.population - a.properties.population; });
-
-    console.log(circles);
-    var circleValue = function(d) { return +d.properties.population; };
-    var projection = 'null';
-
-    var tooltipContent = function(d) {return d.properties;}
+    
+    const position = [39.50, -98.35];
 
     return (
       <div>
-      <MuiThemeProvider>
-       <Card>
-         <CardMedia
-           overlay={<CardTitle title="" subtitle="" />}
-         >
-           <img id="headlinePicture" src="/img/TweetMapHeader.png" className="cover" />
-         </CardMedia>
-         <CardTitle className="center" title="So What Exactly is Fidjjit?" subtitle="(pronounced fi-jit)" />
-         <CardText>
-           <p id="small-text">
-           Fidjjit is a tool to help people visualize and understand information created by the popular social networking
-           site Twitter. Our goal is to help users discover new perceptions through manipulating existing tweet data
-           and displaying it in a unique and meaningful way.
-           </p>
-         </CardText>
-         <CardActions className="buttons">
-         </CardActions>
-       </Card>
-       </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="" subtitle="" />}
+            >
+              <img id="headlinePicture" src="/img/TweetMapHeader.png" className="cover" />
+            </CardMedia>
+            <CardTitle className="center" title="So What Exactly is Fidjjit?" subtitle="(pronounced fi-jit)" />
+            <CardText>
+              <p id="small-text">
+              Fidjjit is a tool to help people visualize and understand information created by the popular social networking
+              site Twitter. Our goal is to help users discover new perceptions through manipulating existing tweet data
+              and displaying it in a unique and meaningful way.
+              </p>
+            </CardText>
+            <CardActions className="buttons">
+            </CardActions>
+          </Card>
+        </MuiThemeProvider>
 
-        <MapBubble
-          width={width}
-          height={height}
-          dataPolygon={dataCounties}
-          polygonClass={polygonClass}
-          dataMesh={dataStates}
-          meshClass={meshClass}
-          domain={domain}
-          dataCircle={circles}
-          circleValue={circleValue}
-          circleClass={'bubble'}
-          projection={projection}
-          tooltipContent={tooltipContent}
-          showGraticule={false}
-          showTooltip={true}
-          showLegend={true}
-        />
+        <Map center={position} zoom={4}>
+          <TileLayer
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <CircleMarker center={position}>
+            <Popup>
+              <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+            </Popup>
+          </CircleMarker>
+        </Map>
         {this.props.children}
       </div>
-
     );
   }
 }
