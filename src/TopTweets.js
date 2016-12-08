400af2bd41211d90cfd204583b92e8e7281b2bf5
jhonrade <jhonrade@uw.edu>
2016-12-07 21:15:38 -0800

    UNDEFINED STATUS OF PROMISESgit add . HELP :(

--- src/TopTweets.js
+++ src/TopTweets.js
@@ -22,60 +22,93 @@ var TopTweets = React.createClass({
       this.setState({searchKeyword: e.target.value})
       // URL of tweets containing the keyword in the text of the tweet, sorted by RT's, returns top 5
       var keywordURL = (baseURL + "search/?q=" + e.target.value + top5 + RTsort);
-      this.grabData(keywordURL);
-    },
 
-    // similar to AJAX call
-    grabData:function(url) {
-      return fetch(url)
-      .then((result) => {
-        var res = result.json()
-        console.log(res)
-        var arr = []
-        arr.map(function(i) {
-          arr.push(res[i].text);
-          console.log(arr)
-        return arr;
-      });
-    });
-  },
-
-
-
-//   //get tweets(containing the keyword)'s API object
-//   $.get(keywordURL).then(function(data) {
-//       // add to array of tweets
-//       this.setState({keywordsTweets: data.statuses})
-//       console.log(data)
-//   }.bind(this))
-// },
+  //get tweets(containing the keyword)'s API object
+  $.get(keywordURL).then(function(data) {
+      // add to array of tweets
+      this.setState({KeywordsTweets: data.statuses})
+  }.bind(this))
+},
 
 //Render the search box, and renders all of the tweets of the inputed keyword's
 render:function() {
   return(
-        <div className= "container" id="TopTweetsResults">
+        <div className="container" id="TopTweetsResults">
           <form>
             <input onChange={this.getKeyword} type="text" id="search_keyword" placeholder="Type in an keyword...."/>
           </form>
 
-              { this.state.keywordsTweets.map(function(m, i) {
-                  return <Tweet
-                    key={'tweet-' + i}
-                    keyword={this.state.searchKeyword}
-                    tweetID={m.id_str}
-                    tweetText={m.text}
-                    user={m.user.screen_name}
-                    profileImage ={m.user.profile_image_url}
-                  />
-                })}
-            </div>
+          <div>
+            { this.state.KeywordsTweets.map(function(m, i) {
+                return <Tweet
+                  key={'tweet-' + i}
+                  keyword={this.state.searchKeyword}
+                  tweetID={m.id_str}
+                  tweetText={m.text}
+                  user={m.user.screen_name}
+                  name={m.user.name}
+                  profileImage={m.user.profile_image_url}
+                />
+              })}
+          </div>
+
         </div>
-      )}
+  )}
 
   });
 
-
-
+  // <div>
+  //   { this.state.KeywordsTweets.map(function(m, i) {
+  //       return <Tweet
+  //         key={'tweet-' + i}
+  //         keyword={this.state.searchKeyword}
+  //         tweetID={m.id_str}
+  //         tweetText={m.text}
+  //         user={m.user.screen_name}
+  //         profileImage ={m.user.profile_image_url}
+  //       />
+  //     })}
+  // </div>
 
 
 export default TopTweets;
+
+
+// getKeyword:function(e) {
+//     this.setState({searchKeyword: e.target.value})
+//     // URL of tweets containing the keyword in the text of the tweet, sorted by RT's, returns top 5
+//     var keywordURL = (baseURL + "search/?q=" + e.target.value + top5 + RTsort);
+//   //   this.grabData(keywordURL);
+//   // },
+//
+//   // similar to AJAX call
+//   // grabData:function(url) {
+//   //   return fetch(url)
+//   //   .then((result) => {
+//   //     // results are a Promise
+//   //     var res = result.json()
+//   //     console.log(res)
+//   //     console.log(res['[[PromiseValue]]'])
+//   //     var arr = []
+//   //     this.setState({KeywordsTweets: ['[[PromiseValue]]'].statuses})
+//   //     console.log(['[[PromiseValue]]'].statuses)
+//   //     arr.map(function(i) {
+//   //       arr.push(res[i].text);
+//   //       console.log(arr)
+//   //     return arr;
+//   //   });
+//   // });
+//
+// //   // Tiff fucking around
+// //   grabData:function(url) {
+// //     return fetch(url)
+// //     .then((result) => {
+// //       // results are a Promise
+// //       var res = result.json()
+// //       var temp = Promise.resolve(res['[[PromiseValue]]']);
+// //       console.log(temp)
+// //   }).then(function(t) {
+// //       console.log(t);
+// //   });
+// //
+// // },
