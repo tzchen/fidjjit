b67b206bf59567c427f3e8ab381ddcb2debb90ae
JILLIAN HONRADE <jhonrade@uw.edu>
2016-12-09 01:41:34 -0800

    fixed comments/style

--- src/Tweet.js
+++ src/Tweet.js
@@ -3,11 +3,8 @@ import './css/Tweet.css';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-var t = "http://twitframe.com/show?url=";
-
- // TEST
+// Tweet component that renders tweet's information
 var Tweet = React.createClass({
-
     render:function() {
         return (
           <div>
@@ -35,42 +32,3 @@ var Tweet = React.createClass({
 
 
 export default Tweet;
-
-// <div className="tweetItem">
-//
-// <div className="RT">
-//     {this.props.RTcount} RTs
-// </div>
-//
-// <img class="profilepic" src={this.props.profileImage} />
-//
-//   <blockquote className="twitter-tweet" data-lang="en">
-//     <p lang="en" dir="ltr">{this.props.tweetText}</p>&mdash; {this.props.name} (@{this.props.username}) <a href={this.props.link}>{this.props.created_at}</a>
-//   </blockquote>
-//
-// </div>
-// </div>
-// )}
-
-// <div className="tweetItem">
-//
-// <h2> user: </h2>
-// <p>{this.props.name} </p>
-//
-// <img src={this.props.profileImage} />
-//
-//
-// <h2> date: </h2>
-// <p>{this.props.created_at} </p>
-//
-//   <h2> tweet: </h2>
-//   <p>{this.props.tweetText}</p>
-//
-//   <h2> RTs: </h2>
-//   <p>{this.props.RTcount} </p>
-//
-//   <blockquote class="twitter-tweet" data-lang="en">
-//     <p lang="en" dir="ltr">{this.props.tweetText}</p>&mdash; {this.props.name} (@{this.props.username}) <a href={this.props.link}>{this.props.created_at}</a>
-//   </blockquote>
-//
-// </div>
